9be7a577d375ca353523a5f000ee81d07288178d
Nagajayasuryaak21 <nagajayasuryaa.11226@gmail.com>
2023-10-27 21:19:50 +0530

    sample

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+const adminSchema = new mongoose.Schema({
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+  });
+
+
+const Admin = mongoose.model("user", adminSchema);
+
+const validate = (data) => {
+	const schema = Joi.object({
+		firstName: Joi.string().required().label("First Name"),
+		lastName: Joi.string().required().label("Last Name"),
+		email: Joi.string().email().required().label("Email"),
+		password: passwordComplexity().required().label("Password"),
+	});
+	return schema.validate(data);
+};
+
+module.exports = {validate ,Admin};
---
+++ routes/auth.js
@@ -0,0 +1,82 @@
+const router = require("express").Router();
+const {Admin } = require("../models/user");
+const bcrypt = require("bcryptjs");
+const Joi = require("joi");
+
+router.get("/", (req, res) => {
+  res.send("WELCOME TO NO-CODE APP API");
+});
+router.post("/log", async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  //console.log(req.body);
+  try {
+    const { error } = validate(req.body);
+    if (error)
+      return res.status(400).send({ message: error.details[0].message });
+
+    const user = await Admin.findOne({ email: req.body.email });
+    if (!user)
+      return res.status(401).send("Invalid Email or Password" );
+    const validPassword = bcrypt.compareSync(req.body.password, user.password);
+    if (!validPassword)
+      return res.status(401).send( "Invalid Email or Password" );
+      
+    res.status(200).send({ message: "logged in successfully",userId:user._id });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" + error.message });
+  }
+});
+
+const validate = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required().label("Email"),
+    password: Joi.string().required().label("Password"),
+  });
+  return schema.validate(data);
+};
+router.post("/register", async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  // console.log(req.body);
+  try {
+    let user = await Admin.findOne({ email: req.body.email });
+    if (user)
+      return res
+        .status(409)
+        .send({ message: "User with given email already Exist!" });
+    // const hashPassword = await bcrypt.hash(req.body.password, salt);
+    const saltRounds = 10;
+    const hashedPassword = bcrypt.hashSync(req.body.password, saltRounds);
+    console.log(hashedPassword);
+    await new Admin({ ...req.body, password: hashedPassword })
+      .save()
+      .then((savedData) => {
+        res.status(200).send({data:savedData,message:"Registered successfully"});
+        
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Internal server error");
+      });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
